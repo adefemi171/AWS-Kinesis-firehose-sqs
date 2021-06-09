28c0fc80c2e8fefac126fa985738b7ed6857d935
adefemi171 <adefemi171@gmail.com>
2021-06-08 20:13:18 -0700

    Adding comment

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket               = "kinesis-data-drop"
-    key                  = "terraform/state/apps/kinesis/kinesis.tf"
+    bucket               = "kinesis-data-drop" // dont forget to edit bucket name
+    key                  = "terraform/state/apps/kinesis/kinesis.tf" 
     workspace_key_prefix = "terraform"
     region               = "us-west-2"
   }
